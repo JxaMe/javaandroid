59ae84f64f52115d40f471cdc6e0d5ae3b299e05
loste <loster0219@gmail.com>
2018-12-20 19:56:14 +0800

    java4android 随手敲代码

--- src/CanonPrinter.java
+++ src/CanonPrinter.java
@@ -1,9 +1,12 @@
-public class CanonPrinter extends Printer{
-    void close(){
+public class CanonPrinter extends Printer {
+    void close() {
         this.clean();
         super.close();
     }
-    void clean(){
+
+    void clean() {
         System.out.println("佳能专用,清理功能!");
+
     }
+
 }
